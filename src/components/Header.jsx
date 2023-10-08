49e5181bf230ce20ee344112f1eaf6ff9ea7bd52
juansev <juansoussev@gmail.com>
2023-10-08 09:17:11 -0300

    REFACTOR: header component added

---
+++ src/components/Header.jsx
@@ -0,0 +1,43 @@
+import { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <header className='header d-flex flex-row justify-content-between'>
+        <Link to='/'>
+        <img className='logo-header' src="/pcf.png" alt="asd"></img>
+        </Link>
+        <nav className="navbar navbar-expand-lg">
+          <div className="container-fluid">
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse bg-semi" id="navbarSupportedContent">
+              <ul className="navbar-nav me-auto  mb-lg-0 mr-5">
+                <li className="nav-item">
+                  <Link className="nav-link" to="/agende-ja">Agende já</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/video-aulas">Video Aulas</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/questoes-ineditas">Questoes Ineditas</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/about">Quem somos</Link>
+                </li>
+              </ul>
+              <br />
+              <form role="search">
+                <input className="form-control me-2" type="search" placeholder="Pesquisar" aria-label="Pesquisar"></input>
+                  <button className="btn btn-outline-warning" type="submit">Pesquisar</button>
+              </form>
+            </div>
+          </div>
+        </nav>
+      </header>
+      
+    )
+  }
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
-import './index.css'
+import Header from './components/Header.jsx'
+import './assets/css/style.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
